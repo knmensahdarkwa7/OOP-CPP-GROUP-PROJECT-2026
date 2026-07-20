533afa200ae999da708802066307ce161435d437
knmensahdarkwa7 <annonymoussilence7@gmail.com>
2026-07-20 14:15:54 +0000

    intial commit  - skeleton of code

---
+++ Library_Mangement.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+
+class Book{
+
+};
+
+class User{
+    private:
+    string name;
+    int age;
+
+    public:
+
+
+};
+
+class Libarian{
+    private:
+    string name;
+
+    public:
+
+
+};
+
+
+int main(){
+
+
+    return 0;
+
+}
+
