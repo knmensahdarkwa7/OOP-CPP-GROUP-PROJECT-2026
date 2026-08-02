62fbec1b133c1330af788fae9f69179d43a8d133
Boahemaaaa <yaaboahemaawiredu14@gmail.com>
2026-08-02 11:24:11 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+void librarianDashboard(Librarian &lib){int choice;
+do{
+        cout<<"n\LIBRARIAN DASHBOARD"<<endl;
+
+        cout<<"1.Add Book "<<endl;
+        cout<<"2.Remove Book "<<endl;
+        cout<<"3.Add Book "<<endl;
+        cout<<"4.Issue Book "<<endl;
+        cout<<"5.Return Book "<<endl;
+        cout<<"5.Return Book "<<endl;
+        cout<<"5.Return Book "<<endl;
+        cout<<"6.Calculate Fine "<<endl;
+        cout<<"7.View all Users"<<endl;
+        cout<<"0.Exit"<<endl;
+        cout<<"Enter Choice: "<<endl;
+        cin>>"choice"<<endl;
+
+
+}
+while(choice!=0);
+}
+
